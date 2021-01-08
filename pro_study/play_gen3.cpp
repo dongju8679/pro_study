b64c877f8c99fa92dbe666f926881803778fecd6
DongJu Ha <dongju8679@hotmail.com>
2021-01-08 17:06:55 +0900

    check play_gen5

--- pro_study/play_gen3.cpp
+++ pro_study/play_gen3.cpp
@@ -18,33 +18,18 @@ struct dat_t {
 struct play_t {
 	list_head play_list;
 	dat_t* dat;
-}play;
+}play[MAX_NUM], ptmp[MAX_NUM];
 
 struct gen_t {
 	list_head gen_list;
 	dat_t* dat;
-}gen;
-
-struct arr_t {
-	play_t* play_ptr;
-	gen_t* list_ptr;
-	dat_t* data;
-};
+}gen[MAX_NUM], gtmp[MAX_NUM];
 
 enum {
 	play_list_enum = 0,
 	gen_list_enum = 1,
 	max_num_list_enum = 2
 };
-
-int offsetoflist[max_num_list_enum] = { 0, sizeof(list_head) };
-
-#if 0
-int alloc_cur;
-dat_t* my_alloc() {
-	return &arr[alloc_cur++];
-}
-#endif
 
 void list_init(list_head* head) {
 	head->prev = head;
@@ -84,14 +69,19 @@ int gen_input(int N) {
 }
 
 int init_data(dat_t* in, dat_t* arr, int N) {
-	int ret = 0;
-	list_t* list_tmp = 0;
+	int ret = 0;	
+	play_t* play_l;
+	gen_t* gen_l;
 	for (int i = 0; i < N; i++) {
 		arr[i].idx = in[i].idx;		
 		arr[i].playtime = in[i].playtime;
 		arr[i].genre = in[i].genre;	
-		list_tmp = &list[i];
-		list_tmp->dat = &arr[i];
+		play_l = &play[i];
+		std::cout << "play_l = " << play_l << std::endl;
+		play_l->dat = &arr[i];
+		std::cout << "play_l->dat = " << play_l->dat << std::endl;
+		gen_l = &gen[i];		
+		gen_l->dat = &arr[i];
 	}
 	return ret;
 }
@@ -162,67 +152,77 @@ void merge_sort_play(dat_t* arr, int start, int end) {
 	while (i <= middle && j <= end) {
 		if (check_condition_play(arr, i, j) == 1) {
 			tmp[k++] = arr[i++];
+			ptmp[k++] = play[i++];			
 		}
 		else {
 			tmp[k++] = arr[j++];
+			ptmp[k++] = play[j++];			
 		}
 	}
 	while (i <= middle) {
 		tmp[k++] = arr[i++];
+		ptmp[k++] = play[i++];		
 	}
 	while (j <= end) {
 		tmp[k++] = arr[j++];
+		ptmp[k++] = play[j++];		
 	}
 	for (k = start; k <= end; k++) {
 		arr[k] = tmp[k];
+		play[k] = ptmp[k];		
 	}
 }
 
 int show_arr(dat_t* arr, int N) {
 	int ret = 0;
 	for (int i = 0; i < N; i++) {
+		std::cout << "&arr[i] = " << &arr[i] << std::endl;
 		std::cout << "arr[" << i << "] = " << arr[i].idx << "/" << arr[i].playtime << "/" << arr[i].genre << std::endl;
 	}
 	return ret;
 }
 
-int make_play_list(dat_t* arr, int N) {
+int make_play_list(int N) {
 	int ret = 0;
-	list_t* target = 0;
+	play_t* target = 0;
 	for (int i = 0; i < N; i++) {
-		dat_t* dat = &arr[i];
-		target = &list[dat->idx];
+		target = &play[i];
+		std::cout << "target = " << target << std::endl;
+		std::cout << "target->dat = " << target->dat << std::endl;
 		list_add_tail(&play_head, &(target->play_list));
-	}
+	}	
 	return ret;
 }
 
 int make_gen_list(dat_t* arr, int N) {
 	int ret = 0;
-	list_t* target = 0;
-	for (int i = 0; i < N; i++) {
-		dat_t* dat = &arr[i];
-		//std::cout << "dat = " << dat << std::endl;
-		int gen_head_idx = dat->genre;
-		target = &list[dat->idx];
+	gen_t* target = 0;
+	int gen_head_idx = 0;
+	for (int i = 0; i < N; i++) {				
+		target = &gen[i];
+		gen_head_idx = target->dat->idx;
 		//std::cout << "gen_head_idx = " << gen_head_idx  << ", idx = " << dat->idx << ", playtime = " << dat->playtime << std::endl;		
 		list_add_tail(&gen_head[gen_head_idx], &(target->gen_list));
 	}
 	return ret;
 }
 
-int for_each_list_safe(list_head* head, int list_idx) {
+int for_each_list_safe(list_head* head) {
 	int ret = 0;
 	list_head* tmp = 0;
-	list_head* n = 0;
-	list_t* target = 0;
+	list_head* n = 0;	
 	int cnt = 0;
-	std::cout << "for_each_list_safe, list_idx" << list_idx << std::endl;
-	for (tmp = head->next, n = tmp->next; tmp != head; tmp = n, n = n->next) {
-		target = reinterpret_cast<list_t*>((char*)tmp - (char*)offsetoflist[list_idx]);
+	play_t* target = 0;
+	std::cout << "for_each_list_safe, head = " << head << std::endl;
+	for (tmp = head->next, n = tmp->next; tmp != head; tmp = n, n = n->next) {	
+		std::cout << "tmp = " << tmp << std::endl;
+		target = reinterpret_cast<play_t*>(tmp);
 		dat_t* arr = target->dat;
-		std::cout << "arr[" << cnt << "] = " << arr->idx << "/" << arr->playtime << "/" << arr->genre << std::endl;
+		std::cout << "arr = " << arr << std::endl;
+#if 1
+		//std::cout << "arr[" << cnt << "] = " << arr->idx << "/" << arr->playtime << "/" << arr->genre << std::endl;
 		cnt++;
+#endif
 	}
 	return ret;
 }
@@ -263,9 +263,9 @@ int update_playtime(int id, int playtime) {
 	list_head* tmp = 0;
 	list_head* n = 0;
 	list_head* head = &play_head;
-	for (tmp = head->next, n = tmp->next; tmp != head; tmp = n, n = n->next) {
-		dat_t* dat = reinterpret_cast<dat_t*>((char*)tmp - (char*)offsetoflist[0]);
-		std::cout << "A, dat->idx = " << dat->idx << std::endl;
+	for (tmp = head->next, n = tmp->next; tmp != head; tmp = n, n = n->next) {		
+		play_t* play_l = (play_t*)tmp;
+		dat_t* dat = play_l->dat;
 		if (dat->idx == id) {
 			std::cout << "dat->idx = " << dat->idx << std::endl;
 			dat->playtime += playtime;
@@ -279,20 +279,17 @@ int main() {
 	int ret = 0;
 	std::cout << "start play_gen1" << std::endl;
 	clock_t start, end;
-
 #if 1
-	//alloc_cur = 0;
 	list_init(&play_head);
 	for (int i = 0; i < N; i++) {
 		list_init(&gen_head[i]);
 	}
 #endif
-
 	N = MAX_NUM;
 	start = clock();
-	gen_input(N);	
+	gen_input(N);
 	end = clock();
-	std::cout << "gen_input, elapsed time = " << end - start << std::endl;	
+	std::cout << "gen_input, elapsed time = " << end - start << std::endl;
 #if 1
 	start = clock();
 	init_data(in, arr, N);
@@ -300,19 +297,21 @@ int main() {
 	std::cout << "init_data, elapsed time = " << end - start << std::endl;
 	show_arr(arr, N);
 #endif
+#if 0
 	start = clock();
 	merge_sort_play(arr, 0, N - 1);
 	end = clock();
 	std::cout << "merge_sort_play, elapsed time = " << end - start << std::endl;
 	show_arr(arr, N);
+#endif
 
-	start = clock();
-	make_play_list(arr, N);
-	for_each_list_safe(&play_head, play_list_enum);
+	start = clock();		
+	make_play_list(N);		
+	for_each_list_safe(&play_head);
 	end = clock();
 	std::cout << "make_play_list, elapsed time = " << end - start << std::endl;
-
 #if 0
+
 	start = clock();
 	merge_sort_gen(arr, 0, N - 1);
 	end = clock();
